c176e76220d527f86007755f1eb2db008a7f936e
wuweiyan <772341828@qq.com>
2019-08-18 21:14:51 +0800

    update song.url

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -42,7 +42,7 @@ export function createSong (musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `https://api.bzqll.com/music/tencent/url?key=579621905&id=${musicData.songmid}&br=320`
   })
 }
 
